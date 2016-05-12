7ca2fdc1164781a72db855282914c533509aa443
michaelfselby <michaelfselby@gmail.com>
2016-05-12 15:26:00 -0700

    Add animated gif

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var peopleField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var personLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        let tipPercentage = [0.05, 0.1, 0.25]
+        let people = Double(peopleField.text!) ?? 0
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage [tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let person = total / people
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        personLabel.text = String(format: "$%.2f", person)
+
+
+    }
 }
 
